bac68f00d63cd0ffba8c38cd860c15959a76e57a
Dimitris Karakostas <dimit.karakostas@gmail.com>
2021-06-19 16:26:38 +0100

    Update agent name

--- config/node.py
+++ config/node.py
@@ -2,4 +2,4 @@ MAJOR, MINOR = 0, 3
 VERSION = '%d.%d.1' % (MAJOR, MINOR)
 VERSION_REGEX = '%d\.%d\.\d+' % (MAJOR, MINOR)
 
-AGENT = 'Penguin-Core Client 0.1'
+AGENT = 'Penguin-Core %d.%d.0' % (MAJOR, MINOR)
