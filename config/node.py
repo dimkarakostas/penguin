88c29fd6bfdaa4583e44ec82cdd14e8d6fcc7eef
Dimitris Karakostas <dimit.karakostas@gmail.com>
2021-06-24 23:43:45 +0100

    Use generic regex to allow connections from old releases

--- config/node.py
+++ config/node.py
@@ -1,6 +1,7 @@
-MAJOR, MINOR = 0, 3
+MAJOR, MINOR = 0, 5
 VERSION = '%d.%d.1' % (MAJOR, MINOR)
-VERSION_REGEX = '%d\.%d\.\d+' % (MAJOR, MINOR)
+# VERSION_REGEX = '%d\.%d\.\d+' % (MAJOR, MINOR)
+VERSION_REGEX = '0\.\d\.\d+'
 
 AGENT = 'Penguin-Core %d.%d.0' % (MAJOR, MINOR)
 
