1bd3f92305e332186d170b5202e28cc668ac01d6
Dimitris Karakostas <dimit.karakostas@gmail.com>
2021-06-26 18:41:48 +0100

    Minor logging change

--- src/blockchain.py
+++ src/blockchain.py
@@ -4,7 +4,7 @@ from hashlib import sha256
 
 def parse_object(obj, node, peer_id):
     obj_id = sha256(canonicalize(obj)).hexdigest()
-    node.log.info('Peer %s sent object %s with id %s' % (peer_id, obj, obj_id))
     if not node.db.get(obj_id):
+        node.log.info('Peer %s sent object %s with id %s' % (peer_id, obj, obj_id))
         node.db.set(obj_id, obj)
         node.broadcast_object(obj_id)
