1acf7bef1674dfb4f2b96055a5ac722997071ac0
Dimitris Karakostas <dimit.karakostas@gmail.com>
2021-06-29 18:59:06 +0100

    Fix tx verification bugs

--- src/blockchain.py
+++ src/blockchain.py
@@ -2,29 +2,34 @@ from library.Canonicalize import canonicalize
 from hashlib import sha256
 from nacl.signing import SigningKey, VerifyKey
 from nacl.encoding import HexEncoder
+from nacl import exceptions as nacl_exceptions
 import logging
 import config
+import copy
 
 
 class UTxO:
     log = logging.getLogger('UTxO')
 
-    def __init__(self, outpoint, node):
+    def __init__(self, outpoint, db):
+        self.db = db
+
         self.tx_id = outpoint['txid']
         self.index = outpoint['index']
 
-        tx = self.node.db.get(self.tx_id)
+        tx = self.db.get(self.tx_id)
         assert tx, 'Transaction %s not in db' % self.tx_id
 
-        self.pubkey = tx['outputs'][self.index]['pubkey']
+        self.pubkey = VerifyKey(tx['outputs'][self.index]['pubkey'], encoder=HexEncoder)
         self.value = tx['outputs'][self.index]['value']
 
 
 class Transaction:
     log = logging.getLogger('Transaction')
 
-    def __init__(self, obj, node, coinbase=False):
+    def __init__(self, obj, db, coinbase=False):
         self.id = sha256(canonicalize(obj)).hexdigest()
+        self.db = db
         if coinbase:
             assert all([
                 'inputs' not in obj,
@@ -35,28 +40,31 @@ class Transaction:
             return
 
         self.conservation = 0
-        self.check_inputs()
+        self.check_inputs(obj)
         self.outputs = obj['outputs']
         for output in self.outputs:
             self.conservation += output['value']
         assert self.conservation <= 0, 'Tx %s does not respect law of conservation' % self.id
 
-    def check_inputs(self):
-        tx = dict(obj)
+        self.valid = True
+
+    def check_inputs(self, obj):
+        tx = copy.deepcopy(obj)
         for inp in tx['inputs']:
             inp['sig'] = None
+
         self.inputs = obj['inputs']
         for inp in self.inputs:
             sig_bytes = bytes(inp['sig'], 'utf-8')
             try:
-                utxo = UTxO(inp['outpoint'], node)
+                utxo = UTxO(inp['outpoint'], self.db)
             except AssertionError:
                 self.log.debug('UTxO not found for tx %s' % inp['outpoint']['txid'])
                 self.valid = False
                 return
             try:
-                utxo.pubkey.verify(canonicalize(tx), HexEncoder.decode(sig_bytes), encoder=HexEncoder)
-            except nacl.exceptions.BadSignatureError:
+                utxo.pubkey.verify(sig_bytes + bytes(canonicalize(tx).hex(), 'utf-8'), encoder=HexEncoder)
+            except nacl_exceptions.BadSignatureError:
                 assert False, 'Invalid signature for tx %s, UTxO index %d' % (self.id, utxo.index)
             self.conservation -= utxo.value
 
@@ -64,27 +72,33 @@ class Transaction:
 class Block:
     log = logging.getLogger('Block')
 
-    def __init__(self, obj, node):
+    def __init__(self, obj, db):
+        self.id = sha256(canonicalize(obj)).hexdigest()
+        self.db = db
+
         self.valid = False
 
 
-def parse_object(obj_dict, node, peer_id):
+def parse_object(obj_dict, db, peer_id, coinbase=False):
+    log = logging.getLogger('Parser')
+    db = node.db
+
     obj_id = sha256(canonicalize(obj_dict)).hexdigest()
     if obj_dict['type'] == 'transaction':
-        node.log.info('%s sent tx %s with id %s' % (peer_id, obj_dict, obj_id))
+        log.info('%s sent tx %s with id %s' % (peer_id, obj_dict, obj_id))
         try:
-            obj = Transaction(obj_dict, node)
+            obj = Transaction(obj_dict, db, coinbase)
         except KeyError:
             assert False, 'Transaction %s is malformed' % obj_id
     elif obj_dict['type'] == 'block':
-        node.log.info('%s sent block %s with id %s' % (peer_id, obj_dict, obj_id))
-        obj = Block(obj_dict, node)
+        log.info('%s sent block %s with id %s' % (peer_id, obj_dict, obj_id))
+        obj = Block(obj_dict, db)
         if obj_id == config.blockchain.GENESIS_ID:
             obj.valid = True
     else:
         assert False, 'Unknown object type with id %s' % obj_id
 
-    if obj.valid and not node.db.get(obj_id):
-        node.log.info('Adding object %s to db' % obj_id)
-        node.db.set(obj_id, obj_dict)
+    if obj.valid and not db.get(obj_id):
+        log.info('Adding object %s to db' % obj_id)
+        db.set(obj_id, obj_dict)
         node.broadcast_object(obj_id)
